4b61d143f3dff37c92ec21d4ae4d2a8bdf7afc30
codercollo <itscollinsmaina@gmail.com>
2026-02-08 20:38:04 +0300

    Added the initial entrypoint for our go applcation

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+//Run - is going to be responsibe for the instantiation and startup of the application
+func Run() error {
+	fmt.Println("starting up our application")
+	return nil
+
+}
+
+func main() {
+	fmt.Println("Go Rest API")
+	if err := Run(); err != nil {
+		fmt.Println(err)
+	}
+}
